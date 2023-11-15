556e487acc10622fd99ea2401ee611a6a4f89d33
Mohammed <126573207+imdshah@users.noreply.github.com>
2023-11-15 10:01:26 +0530

    commit

--- test1.php
+++ test1.php
@@ -1,43 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Page</title>
-    <link rel="stylesheet" type="text/css" href="style1.css">
-</head>
-<body>
-    <div class="taskbar">
-        <ul>
-            <li><a href="home.html">Home</a></li>
-            <li><a href="test1.php">Team</a></li>
-            <li><a href="coach.php">Coach</a></li>
-            <li><a href="matches.php">Matches</a></li>
-            <li><a href="players.php">Players</a></li>
-            <li style="float: right;"><a href="logout.php">Logout</a></li>
-        </ul>
-    </div>
-
-    <div class="content">
-    <h2>Team Search</h2>
-    <form method="post" action="">
-        <label for="teamName" style="font-size: 18px;">Search by Team Name:</label>
-        <input type="text" id="teamName" name="teamName" placeholder="Enter team name" style="font-size: 16px;">
-        <input type="submit" value="Search" style="font-size: 16px;">
-        </form>
-    
-        <br>
-        <h2>Team List</h2>
-        <br><br>
-    <div class="manage-teams">
-    <a class="button" href="ateam.php">Add Team</a>
-    <a class="button" href="dteam.php">Delete Team</a>
-    <a class="button" href="uteam.php">Update Team</a>
-</div>
-    </div>
-</body>
-</html> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -134,6 +94,40 @@
         .search-button:hover {
             background-color: #005599;
         }
+
+        .team-card {
+            border: 1px solid #ccc;
+            margin: 10px;
+            padding: 10px;
+            text-align: center;
+            width: 100px;
+        }
+
+        .team-card img {
+            width: 80px;
+            height: 80px;
+        }
+
+        .manage-teams {
+            margin-top: 20px;
+        }
+
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            font-size: 16px;
+            text-align: center;
+            text-decoration: none;
+            background-color: #004080;
+            color: #fff;
+            border-radius: 5px;
+            margin-right: 10px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .button:hover {
+            background-color: #005599;
+        }
     </style>
 </head>
 <body>
@@ -174,66 +168,45 @@
                 die("Failed to connect with MySQL: ". mysqli_connect_error());
             }
     
-            // Check if the form is submitted
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $searchTerm = $_POST['teamName'];
     
-                
                 $sql = "SELECT `team_id`, `teamname`, `captainname`, `home_ground`, `image` FROM team WHERE teamname LIKE '%$searchTerm%'";
                 $result = $conn->query($sql);
     
                 if ($result->num_rows > 0) {
-                    echo "<table border='1' style='width: 50%; border-collapse: collapse;'>";
-                    echo "<tr style='background-color: #f2f2f2;'><th>Team Logo</th><th>Team Name</th><th>Captain Name</th><th>Home Ground</th></tr>";
-                    
+                    echo "<div style='display: flex; flex-wrap: wrap; justify-content: space-around;'>";
+    
                     while ($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        $img = $row["image"];
-                        $teamId = $row["team_id"];
-                        $imagePath = "images/" . $img; 
-                        echo "<td style='padding: 5px; text-align: center;'><img src=" . $imagePath . " alt='Team Logo' style='width: 80px; height: 80px;'></td>";
-                        echo "<td style='padding: 5px; text-align: center;'><a href='team_details.php?team_id={$row["team_id"]}'>" . $row["teamname"] . "</a></td>";
-                        echo "<td style='padding: 5px; text-align: center;'>" . $row["captainname"] . "</td>";
-                        echo "<td style='padding: 5px; text-align: center;'>" . $row["home_ground"] . "</td>";
-                        echo "</tr>";
+                        echo "<div class='team-card'>";
+                        echo "<a href='team_details.php?team_id={$row["team_id"]}'>";
+                        echo "<img src='images/{$row["image"]}' alt='Team Logo'><br>{$row["teamname"]}</a>";
+                        echo "</div>";
                     }
-                    echo "</table>";
+    
+                    echo "</div>";
                 } else {
                     echo "<p>No teams found</p>";
                 }
     
             } else {
-                
-                $sql = "SELECT `team_id`, `teamname`, `captainname`, `home_ground`, `image` FROM team ";
+                $sql = "SELECT `team_id`, `teamname`, `captainname`, `home_ground`, `image` FROM team";
                 $result = $conn->query($sql);
     
                 if ($result->num_rows > 0) {
                     echo "<div style='display: flex; flex-wrap: wrap; justify-content: space-around;'>";
-                    $i = 1;
     
                     while ($row = $result->fetch_assoc()) {
-                        $img = $row["image"];
-                        $teamname = $row["teamname"];
-                        $imagePath = "images/" . $img; 
-                
-                        echo "<div class='player-card'>";
-                        //echo "<div style='border: 1px solid #ccc; margin: 10px; padding: 10px; text-align: center; width: 100px;'>";
-                        echo "<a href='team_details.php?team_id={$row["team_id"]}'>" . "<img src='$imagePath' alt='Team Logo' style='width: 80px; height: 80px;'><br>$teamname" . "</a>";
+                        echo "<div class='team-card'>";
+                        echo "<a href='team_details.php?team_id={$row["team_id"]}'>";
+                        echo "<img src='images/{$row["image"]}' alt='Team Logo'><br>{$row["teamname"]}</a>";
                         echo "</div>";
-    
-                        if ($i % 4 == 0) {
-                            echo "</div>"; 
-                            echo "<div style='display: flex; flex-wrap: wrap; justify-content: space-around;'>"; // Start a new row
-                        }
-                
-                        $i++; 
                     }
-                
+    
                     echo "</div>";
                 } else {
                     echo "<p>No teams found</p>";
                 }
-                
             }
     
             $conn->close();
@@ -247,4 +220,3 @@
     </div>
 </body>
 </html>
-
