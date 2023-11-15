4efdb6c5b7d5f845e76e03e80bed3225fb41d7f8
Mohammed <126573207+imdshah@users.noreply.github.com>
2023-11-15 19:36:21 +0530

    commit

--- dteam.php
+++ dteam.php
@@ -6,8 +6,8 @@ $db_name = "sports";
 
 $conn = mysqli_connect($host, $user, $password, $db_name);
 
-if(mysqli_connect_errno()) {
-    die("Failed to connect with MySQL: ". mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    die("Failed to connect with MySQL: " . mysqli_connect_error());
 }
 
 $j = 0;
@@ -26,9 +26,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
+
+// Fetch all teams from the database
 $sql = "SELECT * FROM team";
 $result = $conn->query($sql);
-
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +39,127 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Delete Team</title>
     <link rel="stylesheet" type="text/css" href="style1.css">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f7f7f7;
+        }
+
+        .taskbar {
+            background-color: #004080;
+            overflow: hidden;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        .taskbar ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+        }
+
+        .taskbar li {
+            float: left;
+        }
+
+        .taskbar li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 20px;
+            text-decoration: none;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .taskbar li a:hover {
+            background-color: #005599;
+            color: #fff;
+        }
+
+        .taskbar li.logout {
+            margin-left: auto;
+        }
+
+        .content {
+            padding: 30px;
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            margin-top: 20px;
+        }
+
+        h2 {
+            color: #004080;
+            margin-bottom: 20px;
+        }
+
+        form {
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+            gap: 15px;
+            justify-content: center;
+            align-items: center;
+            text-align: left;
+        }
+
+        label {
+            font-size: 16px;
+            margin-bottom: 5px;
+            color: #333;
+        }
+
+        input {
+            padding: 10px;
+            width: 100%;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
+        input[type="submit"] {
+            background-color: #004080;
+            color: #fff;
+            cursor: pointer;
+            transition: background-color 0.3s, color 0.3s;
+            margin-top: 20px;
+            padding: 12px;
+            border: none;
+            border-radius: 5px;
+            font-size: 18px;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #005599;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        table, th, td {
+            border: 1px solid #ddd;
+        }
+
+        th, td {
+            padding: 10px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        img {
+            width: 80px;
+            height: 80px;
+        }
+    </style>
 </head>
 <body>
     <div class="taskbar">
@@ -85,7 +207,6 @@ $result = $conn->query($sql);
             }
         }
         ?>
-
     </div>
 </body>
 </html>
